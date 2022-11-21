4dfb0c68f60e9745e9e91932a3f4d8da9e95ab9a
EgyptianCowboy <sil.g.vaes@gmail.com>
2022-11-21 08:15:15 +0100

    Bigger parbox

--- ox-report.el
+++ ox-report.el
@@ -295,9 +295,9 @@ headheight=\\baselineskip]{geometry}
     \\rowcolor{white}
       \\parbox{\\linewidth}{{\\@labeltext \\@initiatorlabel}\\\\\\@initiator}
       & \\parbox{\\linewidth}{{\\@labeltext \\@authorlabel}\\\\\\@author}
-      & \\raisebox{-1cm}{\\begin{sideways}\\parbox{2cm}{\\raggedright\\@labeltext\\@presentlabel}\\end{sideways}}
-      & \\raisebox{-1cm}{\\begin{sideways}\\parbox{2cm}{\\raggedright\\@labeltext\\@absentlabel}\\end{sideways}}
-      & \\raisebox{-1cm}{\\begin{sideways}\\parbox{2cm}{\\raggedright\\@labeltext\\@excusedlabel}\\end{sideways}}\\\\
+      & \\raisebox{-1cm}{\\begin{sideways}\\parbox{3cm}{\\raggedright\\@labeltext\\@presentlabel}\\end{sideways}}
+      & \\raisebox{-1cm}{\\begin{sideways}\\parbox{3cm}{\\raggedright\\@labeltext\\@absentlabel}\\end{sideways}}
+      & \\raisebox{-1cm}{\\begin{sideways}\\parbox{3cm}{\\raggedright\\@labeltext\\@excusedlabel}\\end{sideways}}\\\\
     \\rowcolor{white} \\multicolumn{5}{@{}c@{}}{\\@separator}\\\\
     \\rowcolor{white} \\@labeltext \\@participantslabel\\\\
     \\@participantstable
